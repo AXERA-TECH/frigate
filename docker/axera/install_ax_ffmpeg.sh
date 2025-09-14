8d6d11395b3b4c1430e56ec9daf9dae342e32415
shizhicheng <shizhicheng@axera-tech.com>
2025-09-14 09:03:45 +0000

    Updated FFmpeg to improve decoding compatibility

--- docker/axera/install_ax_ffmpeg.sh
+++ docker/axera/install_ax_ffmpeg.sh
@@ -3,13 +3,7 @@
 if [[ "${TARGETARCH}" == "arm64" ]]; then
     #ffmpeg ax
     mkdir -p /usr/lib/ffmpeg/ax
-    wget -qO ffmpeg.tar.gz "https://github.com/ivanshi1108/assets/releases/download/3.6.2/ffmpeg-ax-7.1-121cbba3a-yuv420p.tar.gz"
+    wget -qO ffmpeg.tar.gz "https://github.com/ivanshi1108/assets/releases/download/3.6.2/ffmpeg-ax-7.1-e463d11f1-yuv420p.tar.gz"
     tar -xzf ffmpeg.tar.gz -C /usr/lib/ffmpeg/ax bin/
     rm -f ffmpeg.tar.gz
-
-    # #axera lib
-    # wget -qO lib.tar.gz "https://github.com/ivanshi1108/assets/releases/download/3.6.2/lib.tar.gz"
-    # tar -xzf lib.tar.gz -C /usr/lib/ --strip-components 1
-    # rm -f lib.tar.gz
-    # wget -P /usr/lib/ "https://github.com/ivanshi1108/assets/releases/download/3.6.2/libexif.so"
 fi
